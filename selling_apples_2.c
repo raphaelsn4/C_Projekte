da225a8bdd78778c89a71df0027e52deb16555c4
Raphael <106547577+raphaelsn4@users.noreply.github.com>
2024-06-25 08:49:00 +0200

    Create selling_apples_2.c
    Ich verbessere kontinuierlich den Code für ein Projekt eines Apfelladens, ein Programm, das in der Programmiersprache C entwickelt wurde. Das Ziel ist es, eine effektive Strategie zur Steigerung der Verkäufe zu implementieren, indem die Wochentage mit geringerem Kundenverkehr identifiziert werden. Zudem berücksichtige ich Kundenrezensionen, um das Einkaufserlebnis zu verbessern. 
    
    I am continuously refining the code for a project involving an apple shop, a program developed in the C programming language. The goal is to implement an effective strategy to increase sales by identifying the days of the week with lower customer traffic. Additionally, I consider customer reviews to enhance the shopping experience. 
    
    Estou continuamente aprimorando o código de um projeto da lojinha de maçãs, um programa desenvolvido em linguagem C. O objetivo é implementar uma estratégia eficaz para aumentar as vendas, identificando os dias da semana com menor movimento. Além disso, levo em consideração os reviews dos clientes para melhorar a experiência de compra. Projetos como este me proporcionam um ambiente ideal para aplicar e expandir minhas habilidades.

---
+++ selling_apples_2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+  
+ int appleQuantity;
+ double applePrice = 1.49;
+ double appleReview;
+ int appleReviewDisplay;
+ const char appleLocation = 'F';
+ int dayOfWeek = 0; 
+
+ appleQuantity = 23;
+ appleReview = (823 + 52)/10;
+ 
+ appleReviewDisplay = appleReview;
+ dayOfWeek = 3;
+
+ if(appleQuantity < 10 || dayOfWeek % 7 == 3)
+ {
+  printf("Sale on apples today, today only they are: $%.2f\n", applePrice * .9);
+ }
+
+ 
+ printf("An apple costs: $%.2f, there are %d in inventory found in section: %c and your customers gave it an average review of %d%%!", applePrice, appleQuantity, appleLocation, appleReviewDisplay);
+
+}
